b398322eb4939c9d5647e5da00ee415afafe3415
Daniel Sattler Cantons <dexattler@gmail.com>
2016-04-10 22:12:21 +0200

    Primera versión con 4 cositas añadidas (búsqueda binaria) y algunas anotaciones en el resto de funciones

--- clasesCompartidas/src/clasesCompartidas/ConjuntoNodos.java
+++ clasesCompartidas/src/clasesCompartidas/ConjuntoNodos.java
@@ -15,36 +15,65 @@ public class ConjuntoNodos
 	
 	public void anadir_nodo(int id_nodo)
 	{
-		
+		/*compruebas que no exista
+		 * le das nombre
+		 * lo añades
+		 * comprobar si el id es el de ID_disponible
+		 * en ese caso, hacer un ++ del atributo*/
 	}
 	
 	public void eliminar_nodo(int id_nodo)
 	{
-		
+		/*compruebas que exista
+		 * le das nombre
+		 * lo eliminas*/
 	}
 	
 	public void modificar_nodo(int id_nodo)
 	{
-		
+		/*llama a funciones de la clase nodo*/
 	}
 	
 	public String consultar_nombre_nodo(int id_nodo)
 	{
-		return "result"; //pongo esto solo para que eclipse no me lanze errores
+		/*llama a funciones de la clase nodo*/
+		return "result"; //pongo esto solo para que eclipse no me lance errores
 	}
 	
-	public boolean existe_nodo(int id_nodo)
+	public int existe_nodo(int id_nodo)
 	{
-		return true;//pongo esto solo para que eclipse no me lanze errores
+		int posicion = busqueda_binaria(id_nodo);
+		return posicion;//pongo esto solo para que eclipse no me lance errores
 	}
 	
 	public int consultar_nodo_libre()
 	{
-		return 0; //pongo esto solo para que eclipse no me lanze errores
+		/*if(cola no está vacía)
+		 * return posicion de la cola;
+		 * else*/
+		return ID_nodo_libre; //pongo esto solo para que eclipse no me lance errores
 	}
 	
-	public void liberar_nodo(int id)
+	public void liberar_nodo(int id_nodo)
 	{
-		
+		/*añadir el ID a la cola de disponibles
+		 * borrar nodo*/
+	}
+	
+	/*búsqueda binaria, nos sirve para saber si está o no el id en el conjunto de nodos.*/
+	private static int busqueda_binaria(int id_nodo)
+	{
+		//acceder al nodo con un .getID() o algo así.
+		 int inicio = 0;
+		 int fin = nodos.size()-1;
+		 int posicion;
+		 while(inicio <= fin)
+		 {
+		     posicion = (inicio+fin)/2;		    
+		     if(nodos.get(posicion).consultar_id() == id_nodo) return posicion; //"nodos.get(posicion)" equivale a "nodos[posicion]".
+		     else if(nodos.get(posicion).consultar_id() < id_nodo) inicio = posicion+1;
+		     else fin = posicion-1;     
+		 }
+		 return -1;
 	}
 }
