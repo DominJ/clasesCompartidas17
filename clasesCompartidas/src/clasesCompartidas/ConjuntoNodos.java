1d09d52e33e6494d2c5ec76d9091df93f78ac3d5
Daniel Sattler Cantons <dexattler@gmail.com>
2016-04-13 14:25:33 +0200

    Modificado para hashmaps
    Se han borrado muchos métodos como consecuencia del paso de arraylist a hashmaps. Sólo faltan las pre/post y determinar la estructura que recibirá a la hora de llamar al método "llenar".

--- clasesCompartidas/src/clasesCompartidas/ConjuntoNodos.java
+++ clasesCompartidas/src/clasesCompartidas/ConjuntoNodos.java
@@ -1,132 +1,93 @@
 package clasesCompartidas;
 
-import java.util.Queue;
-import java.util.ArrayList;
+import java.util.*;
 
 public class ConjuntoNodos 
 {
-	private int ID_nodo_libre; //tengo que sacar este dato del fichero inicial.
-	private Queue<Integer> ID_nodos_vacios;
-	private ArrayList<Nodo> nodos;
-	private ArrayList<String> nombres_nodos;
+	private int ID_libre; //tengo que sacar este dato del fichero inicial.
+	private Map<Integer, String> nodos;
+	private Map<String, Integer> nombres_nodos = new HashMap<String, Integer>();
 	
-	
+	/*PRE:*/
+	/*POST:*/
 	public ConjuntoNodos() //constructor por defecto
 	{
-		nodos = new ArrayList<Nodo>();
-		nombres_nodos = new ArrayList<String>();
-		//ID_nodos_vacios = new Queue<Integer>();
+		nodos = new HashMap<Integer, String>();
+		nombres_nodos = new HashMap<String, Integer>();
 	}
 	
-	public void anadir_nodo(Nodo nodo)
+	/*PRE:*/
+	/*POST:*/
+	public void anadir_nodo(String nombre_nodo)
 	{
-		String nombre_nodo = nodo.consultar_nombre();
-		
-		if(existe_nodo(nombre_nodo) != -1) //compruebas que no exista POR NOMBRE
+		if(nombres_nodos.containsKey(nombre_nodo)) //compruebas que no exista POR NOMBRE
 		{
-			//no se puede añadir, se envia un mensaje diciendo que ya existe.
+			//no se puede aÃ±adir, se envia un mensaje diciendo que ya existe.
 			System.out.println("Ya existe un nodo con este nombre");
 		}
-		else //no existe,se añade.
+		else //no existe,se aÃ±ade.
 		{
-			int id_adjudicada = consultar_id_libre(); //se busca la id disponible
-			if(id_adjudicada == ID_nodo_libre) ++ID_nodo_libre; //si se ha usado el nodo libre más alto, se incrementa en 1 para el proximo nodo a añadir.
-			nodo.anadir_id(id_adjudicada); //se le añade la id al nodo.
-			int posicion = colocacion(id_adjudicada); //se calcula en qué posicion se añadirá el nodo para mantener la lista ordenada.
-			nodos.add(posicion,nodo);//se añade el nodo a la lista de nodos.
-			int posicion2 = colocacion_nombre(nombre_nodo);// se calcula en qué posición se añadirá el nombre del nodo para mantener la lista ordenada.
-			nombres_nodos.add(posicion2, nombre_nodo); //se añade el nombre del nodo a la lista de nombres de nodos.
-					
+			int id = ID_libre;
+			++ID_libre;
+			nodos.put(id, nombre_nodo);
+			nombres_nodos.put(nombre_nodo, id);
 		}
 	}
 	
-	public void eliminar_nodo(Nodo nodo)
+	
+	/*PRE:*/
+	/*POST:*/
+	public void eliminar_nodo(int id)
 	{
-		int id_nodo = nodo.consultar_id();
-		if(existe_nodo(id_nodo)!= -1)
+		if(nodos.containsKey(id))//si existe
 		{
-			nombres_nodos.remove(nodo.consultar_nombre());
-			nodos.remove(nodo);
-			ID_nodos_vacios.add(id_nodo);
+			nombres_nodos.remove(nodos.get(id)); //lo borro de nombres_nodos
+			nodos.remove(id); //lo borro de nodos
 		}
-		else
+		else //no existe
 		{
-			System.out.println("No existe ningún nodo con este nombre.");
+			System.out.println("No existe ningÃºn nodo con este nombre.");
 		}
 
 	}
-		
-	public String consultar_nombre_nodo(Nodo nodo)
+
+	/*PRE:*/
+	/*POST:*/
+	private void fijar_id_libre(int id_libre)
 	{
-		String nombre = nodo.consultar_nombre();
-		return nombre;
+		this.ID_libre = id_libre;
 	}
 	
-	private int existe_nodo(int id_nodo)
+	/*PRE:*/
+	/*POST:*/
+	public boolean existe_nombre_nodo(String nombre_nodo)
 	{
-		int posicion = busqueda_binaria(id_nodo);
-		return posicion;
+		boolean existe = false;
+		if(nombres_nodos.get(nombre_nodo)!= null) existe = true;
+		return existe;
 	}
 	
-	private int existe_nodo(String nombre_nodo)
+	/*PRE:*/
+	/*POST:*/
+	public void llenar_conjunto(int total, estructuradenodos)
 	{
-		int posicion = busqueda_binaria_nombre(nombre_nodo);
-		return posicion;//analogamente a su versión de enteros, pero ordenado por nombre.
-	}
-	
-	public int consultar_id_libre()
-	{
-		/*if(cola no está vacía)
-		 * return posicion de la cola;
-		 * else*/
-		return ID_nodo_libre; //pongo esto solo para que eclipse no me lance errores
-	}
-	
-	/*búsqueda binaria, nos sirve para saber si está o no el id en el conjunto de nodos.*/
-	private int busqueda_binaria(int id_nodo)
-	{
-		 int inicio = 0;
-		 int fin = nodos.size()-1;
-		 int posicion;
-		 while(inicio <= fin)
-		 {
-		     posicion = (inicio+fin)/2;		    
-		     if(nodos.get(posicion).consultar_id() == id_nodo) return posicion; //"nodos.get(posicion)" equivale a "nodos[posicion]".
-		     else if(nodos.get(posicion).consultar_id() < id_nodo) inicio = posicion+1;
-		     else fin = posicion-1;     
-		 }
-		 return -1;
-	}
-	
-	private int busqueda_binaria_nombre(String nombre_nodo)
-	{
-		//analogamente a su versión con entero, pero con string.
-		int inicio = 0;
-		int fin = nombres_nodos.size()-1;
-		int posicion;
-		while(inicio <= fin)
+		//presupongo que esa estructura estarÃ¡ ordenada
+		for(int i = 0; i < total-1; ++i)
 		{
-		    posicion = (inicio+fin)/2;		    
-		    if(nombres_nodos.get(posicion).equals(nombre_nodo)) return posicion; //"nodos.get(posicion)" equivale a "nodos[posicion]".
-		    else if(nombres_nodos.get(posicion).compareTo(nombre_nodo) < 0) inicio = posicion+1;
-		    else fin = posicion-1;     
+			anadir_nodo(loquetoque);
 		}
-		return -1;
+		int id = iddelultimonodoaaÃ±adir;
+		fijar_id_libre(id);
+		anadir_nodo(ultimo);
+		//aqui aÃ±adimos tambiÃ©n la Ãºltima de todas, suponiendo que es la ID mÃ¡s alta de todas.
+		
+		
 	}
 	
-	private int colocacion(int id_nodo)
+	/*PRE:*/
+	/*POST:*/
+	public Map<Integer, String> devolver_conjunto()
 	{
-		int posicion;
-		//lleva a cabo una búsqueda binaria para saber dónde se debe colocar el id en cuestion.
-		return posicion;
-	}
-	
-	private int colocacion_nombre(String nombre_nodo)
-	{
-		int posicion;
-		//lleva a cabo una búsqueda binaria para saber dónde se debe colocar el nombre en cuestion.
-		return posicion;
-	}
-	
-	
+		return nodos;
+	}	
 }
