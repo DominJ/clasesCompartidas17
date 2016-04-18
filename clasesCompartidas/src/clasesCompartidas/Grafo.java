eaae7f93d1b1496f15726879337232be6800a91e
Domin <Domin@192.168.1.33>
2016-04-18 21:11:34 +0200

    cabeceras de grafo

--- clasesCompartidas/src/clasesCompartidas/Grafo.java
+++ clasesCompartidas/src/clasesCompartidas/Grafo.java
@@ -2,5 +2,28 @@ package clasesCompartidas;
 
 public class Grafo 
 {
+	public Grafo()
+	{
+		
+	}
 	
+	public void leerDataSet()
+	{
+		
+	}
+	
+	public void escribirDataSet()
+	{
+		
+	}
+	
+	public void leerRelaciones()
+	{
+		
+	}
+	
+	public void escribirRelaciones()
+	{
+		
+	}
 }
