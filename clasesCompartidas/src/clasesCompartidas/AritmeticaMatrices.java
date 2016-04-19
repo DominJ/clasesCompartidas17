47afd3dd5ab9975de5df9fc43749df9cd70ba8b8
klooner <victorhugola_9@hotmail.com>
2016-04-19 21:35:22 +0200

    Update AritmeticaMatrices.java

--- clasesCompartidas/src/clasesCompartidas/AritmeticaMatrices.java
+++ clasesCompartidas/src/clasesCompartidas/AritmeticaMatrices.java
@@ -7,7 +7,7 @@ public interface AritmeticaMatrices
 {
 	public HashMap<Integer,ArrayList<Pair<Integer,Double>>> producto_mat(HashMap<Integer,ArrayList<Pair<Integer,Double>>> a, HashMap<Integer,ArrayList<Pair<Integer,Double>>> b);
 
-	public HashMap<Integer,ArrayList<Pair<Integer,Double>>> HashMap<Integer,ArrayList<Pair<Integer,Double>>> a, HashMap<Integer,ArrayList<Pair<Integer,Double>>> b);
+	public HashMap<Integer,ArrayList<Pair<Integer,Double>>> producto_normalizado(HashMap<Integer,ArrayList<Pair<Integer,Double>>> a, HashMap<Integer,ArrayList<Pair<Integer,Double>>> b);
 
 	public void Relación_Dummy(HashMap<Integer,ArrayList<Pair<Integer,Double>>> M1, HashMap<Integer,ArrayList<Pair<Integer,Double>>> M2, HashMap<Integer,ArrayList<Pair<Integer,Double>>> R);
 
