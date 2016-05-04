7f247caf7bbe91a03df96f67b4a5f6fb3db9e260
marcmendez <marcmendezroca@gmail.com>
2016-05-04 11:09:48 +0200

    Update ConjuntoNodos.java
    Ja no fa falta pasar-li el ID libre a la funcio de crear cjt nodos

--- clasesCompartidas/src/clasesCompartidas/ConjuntoNodos.java
+++ clasesCompartidas/src/clasesCompartidas/ConjuntoNodos.java
@@ -1,15 +1,15 @@
-package clasesCompartidas;
+package clasescompartidas;
 
 import java.util.*;
 
 public class ConjuntoNodos 
 {
 	private int ID_libre; //tengo que sacar este dato del fichero inicial. //inicializar a 1 al comienzo.
-	private Map<Integer, String > nodos;
-	private Map<String, Integer> nombres_nodos;
+	private HashMap<Integer, String > nodos;
+	private HashMap<String, Integer> nombres_nodos;
 	
 	/*PRE: Cierto*/
-	/*POST: Se crea un conjunto de nodos vacío*/
+	/*POST: Se crea un conjunto de nodos vacio*/
 	public ConjuntoNodos() //constructor por defecto
 	{
 		nodos = new HashMap<Integer, String>();
@@ -17,25 +17,31 @@ public class ConjuntoNodos
 		ID_libre = 1;
 	}
 	
-	/*PRE: los hashmaps no son vacíos e ID_libre es un número entero positivo.*/
-	/*POST: Se crea un conjunto de nodos con los valores de los atributos indicados por los parámetros.*/
-	public ConjuntoNodos(Hashmap<int, String> nodos, Hashmap<String, int> nombres_nodos, int ID_libre)
+	/*PRE: los hashmaps no son vacios e ID_libre es un numero entero positivo.*/
+	/*POST: Se crea un conjunto de nodos con los valores de los atributos indicados por los parametros.*/
+	public ConjuntoNodos(HashMap<Integer, String> nodos, HashMap<String, Integer> nombres_nodos)
 	{
 		this.nodos = nodos;
 		this.nombres_nodos = nombres_nodos;
-		this.ID_libre = ID_libre;
+		int aux = 0;
+		for(Integer id: nodos.keySet())
+		{	
+			if(id > aux)
+				aux = id;
+		}
+		this.ID_libre = aux + 1;
 	}
 	
 	/*PRE: No existe ningun nodo con ese nombre en el grafo*/
-	/*POST: Se añade el nodo con el nombre pasado por parámetro al grafo*/
+	/*POST: Se anade el nodo con el nombre pasado por parametro al grafo*/
 	public void anadir_nodo(String nombre_nodo)
 	{
 		if(nombres_nodos.containsKey(nombre_nodo)) //compruebas que no exista POR NOMBRE
 		{
-			//no se puede aÃ±adir, se envia un mensaje diciendo que ya existe.
+			//no se puede añadir, se envia un mensaje diciendo que ya existe.
 			System.out.println("Ya existe un nodo con este nombre");
 		}
-		else //no existe,se aÃ±ade.
+		else //no existe,se añade.
 		{
 			int id = ID_libre;
 			++ID_libre;
@@ -47,48 +53,49 @@ public class ConjuntoNodos
 	
 	/*PRE: El nodo identificado por id existe previamente en el grafo*/
 	/*POST:El nodo identificado por id es eliminado del grafo*/
-	public void eliminar_nodo(int id)
+	public void eliminar_nodo(String id)
 	{
-		if(nodos.containsKey(id))//si existe
+		if(nombres_nodos.containsKey(id))//si existe
 		{
-			nombres_nodos.remove(nodos.get(id)); //lo borro de nombres_nodos
-			nodos.remove(id); //lo borro de nodos
+			nodos.remove(nombres_nodos.get(id)); //lo borro de nombres_nodos
+			nombres_nodos.remove(id); //lo borro de nodos
 		}
 		else //no existe
 		{
-			System.out.println("No existe ningÃºn nodo con este nombre.");
+			System.out.println("No existe ningun nodo con este nombre.");
 		}
 
 	}
 
 	
 	/*PRE:Cierto*/
-	/*POST: Devuelve true si existe un nodo con el nombre pasado por parámetro, false en caso contrario*/
+	/*POST: Devuelve true si existe un nodo con el nombre pasado por parametro, false en caso contrario*/
 	public boolean existe_nodo(String nombre_nodo)
 	{
 		boolean existe = false;
-		if(nombres_nodos.get(nombre_nodo)!= null) existe = true;
+		if(nombres_nodos.containsKey(nombre_nodo)) existe = true;
 		return existe;
 	}
 	
 	/*PRE:Cierto */
-	/*POST:Devuelve true si existe un nodo con el id pasado por parámetro, false en caso contrario*/
+	/*POST:Devuelve true si existe un nodo con el id pasado por parametro, false en caso contrario*/
 	public boolean existe_nodo(int id_nodo)
 	{
 		boolean existe = false;
-		if(nodos.get(id_nodo)!= null) existe = true;
+		if(nodos.containsKey(id_nodo)) existe = true;
 		return existe;
 	}
 
 	/*PRE: nuevo_nombre no existe previamente en el grafo, nombre_nodo existe previamente*/
-	/*POST: Se modifica el nombre del nodo con el nuevo nombre pasado por parámetro.*/
+	/*POST: Se modifica el nombre del nodo con el nuevo nombre pasado por parametro.*/
 	public boolean modificar_nodo(String nombre_nodo, String nuevo_nombre)
 	{	
 		boolean modificado = false;
-		int id = nombres_nodos.get(nombre_nodo);
-		if(id != null)//si existe
+		if(nombres_nodos.containsKey(nombre_nodo))
 		{
-			if(nombres_nodos.get(nuevo_nombre) == null) //si no existe.
+			int id = nombres_nodos.get(nombre_nodo);
+		
+			if(!nombres_nodos.containsKey(nuevo_nombre)) //si no existe.
 			{
 				modificado = true;
 				nodos.remove(id);//borras el nodo para agregarlo con el nuevo nombre
@@ -101,7 +108,7 @@ public class ConjuntoNodos
 	}
 	
 	/*PRE:Cierto*/
-	/*POST: Devuelve el id del nombre del nodo pasado por parámetro*/
+	/*POST: Devuelve el id del nombre del nodo pasado por parametro*/
 	public int consultar_nodo(String nombre_nodo)
 	{
 		int id = nombres_nodos.get(nombre_nodo);
@@ -109,7 +116,7 @@ public class ConjuntoNodos
 	}
 	
 	/*PRE:Cierto*/
-	/*POST: Devuelve el nombre del id del nodo pasado por parámetro*/
+	/*POST: Devuelve el nombre del id del nodo pasado por parametro*/
 	public String consultar_nodo(int id_nodo)
 	{
 		String nombre = nodos.get(id_nodo);
@@ -118,11 +125,12 @@ public class ConjuntoNodos
 
 	
 	/*PRE:Cierto*/
-	/*POST: Devuelve el conjunto de nodos entero del parámetro implícito*/
-	/*Nota: Ésta función está pensada para pasar la información necesaria para poder guardar los datos en un fichero al final de la 
-	 * ejecución del programa.*/
-	public Hashmap<Integer, String> devolver_conjunto()
+	/*POST: Devuelve el conjunto de nodos entero del parametro implicito*/
+	/*Nota: esta funcion esta pensada para pasar la informacion necesaria para poder guardar los datos en un fichero al final de la 
+	 * ejecucion del programa.*/
+	public HashMap<Integer, String> devolver_conjunto()
 	{
 		return nodos;
 	}	
 }
+
